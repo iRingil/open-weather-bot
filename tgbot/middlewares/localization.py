2b2d1b188945ee9d9b67d6e689ae3f31812afa25
iRingil <91410075+iRingil@users.noreply.github.com>
2025-12-27 05:18:17 +0200

    feat: add Postgres, Redis, webhooks

--- tgbot/middlewares/localization.py
+++ tgbot/middlewares/localization.py
@@ -1,8 +1,9 @@
-"""Enables localization"""
+"""Enables localization."""
 
 from aiogram.contrib.middlewares.i18n import I18nMiddleware
 
 from tgbot.config import LOCALES_DIR
 
+__all__: tuple[str] = ("i18n",)
 
-i18n: I18nMiddleware = I18nMiddleware(domain="tgbot", path=LOCALES_DIR)
+i18n: I18nMiddleware = I18nMiddleware(domain="tgbot", path=str(LOCALES_DIR))
