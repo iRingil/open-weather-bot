d45616a65889fc52724fda0e8bbc489652aa43db
iRingil <91410075+iRingil@users.noreply.github.com>
2025-12-27 05:32:12 +0200

    feat: add Postgres, Redis, webhooks

--- tgbot/misc/states.py
+++ tgbot/misc/states.py
@@ -1,9 +1,11 @@
-"""Describes the state for the FSM (Final State Machine)"""
+"""Describes the state for the FSM (Final State Machine)."""
 
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
+__all__: tuple[str] = ("WeatherSetupDialog",)
+
 
 class WeatherSetupDialog(StatesGroup):
-    """Describes the steps of the weather setup dialog"""
+    """Describes the state of the weather setup dialog."""
 
     EnterCityName = State()
