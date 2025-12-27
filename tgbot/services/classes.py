857a0953cd158fcc1600ea3bc5a63614126557c0
iRingil <91410075+iRingil@users.noreply.github.com>
2025-12-27 06:11:58 +0200

    feat: add Postgres, Redis, webhooks

--- tgbot/services/classes.py
+++ tgbot/services/classes.py
@@ -1,17 +1,32 @@
-"""Classes for working with data"""
+"""Classes for working with data."""
 
 from typing import NamedTuple
 
+__all__: tuple[str, ...] = ("CityData", "CurrentWeatherData", "ForecastData", "User", "UserWeatherSettings")
+
 
 class User(NamedTuple):
-    """A class that describes a user"""
+    """
+    A class that describes a user.
+
+    :param id: User id.
+    :param dialog_id: Last dialogue message id.
+    """
 
     id: int
     dialog_id: int
 
 
 class UserWeatherSettings(NamedTuple):
-    """A class that describes the user's weather settings"""
+    """
+    A class that describes the user's weather settings.
+
+    :param lang: ISO 639-1 user language code.
+    :param city: Selected city name.
+    :param latitude: Selected city latitude.
+    :param longitude: Selected city longitude.
+    :param units: Measurement units (metric or imperial).
+    """
 
     lang: str
     city: str
@@ -21,7 +36,14 @@ class UserWeatherSettings(NamedTuple):
 
 
 class CityData(NamedTuple):
-    """A class describing city data"""
+    """
+    A class describing city data.
+
+    :param name: City name.
+    :param full_name: Full city name with country and state (if available).
+    :param latitude: City latitude.
+    :param longitude: City longitude.
+    """
 
     name: str
     full_name: str
@@ -30,7 +52,23 @@ class CityData(NamedTuple):
 
 
 class CurrentWeatherData(NamedTuple):
-    """A class describing current weather data"""
+    """
+    A class describing current weather data.
+
+    :param temp: Temperature in Celsius.
+    :param feels_like: Temperature that feels like in Celsius.
+    :param weather_code: Weather condition code.
+    :param weather_description: Description of weather condition.
+    :param wind_speed: Wind speed in meters per second.
+    :param gust: Wind gust in meters per second.
+    :param humidity: Humidity in percent.
+    :param pressure: Atmospheric pressure in millibars.
+    :param visibility: Visibility in kilometers or None.
+    :param precipitation: Precipitation in millimeters or None.
+    :param time: Time of the measurement.
+    :param sunrise: Sunrise time in UTC.
+    :param sunset: Sunset time in UTC.
+    """
 
     temp: int
     feels_like: int
@@ -40,7 +78,7 @@ class CurrentWeatherData(NamedTuple):
     gust: int | None
     humidity: int
     pressure: int
-    visibility: float
+    visibility: float | None
     precipitation: float | None
     time: str
     sunrise: str
@@ -48,7 +86,14 @@ class CurrentWeatherData(NamedTuple):
 
 
 class ForecastData(NamedTuple):
-    """A class describing weather forecast data"""
+    """
+    A class describing weather forecast data.
+
+    :param time: List of times of the measurements.
+    :param ico_code: List of weather condition codes.
+    :param temp: List of temperatures in Celsius.
+    :param wind_speed: List of wind speeds in meters per second.
+    """
 
     time: list[str]
     ico_code: list[str]
