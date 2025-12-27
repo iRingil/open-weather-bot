6f11b7639af72aaa7cff02048e3daafb54289a59
iRingil <91410075+iRingil@users.noreply.github.com>
2025-12-27 03:25:22 +0200

    feat: add Postgres, Redis, webhooks

--- tgbot/filters/admin.py
+++ tgbot/filters/admin.py
@@ -1,4 +1,4 @@
-""" Allows to perform actions if the user is an administrator """
+"""Allows to perform actions if the user is an administrator."""
 
 from aiogram.dispatcher.filters import BoundFilter
 from aiogram.types import CallbackQuery, Message
@@ -6,16 +6,29 @@ from aiogram.types import CallbackQuery, Message
 from tgbot.config import Config
 
 
+__all__: tuple[str] = ("AdminFilter",)
+
+
 class AdminFilter(BoundFilter):
-    """Allows to define administrators"""
+    """Allows to define administrators."""
 
-    key = "is_admin"
+    key: str = "is_admin"
 
-    def __init__(self, is_admin: bool | None = None):
+    def __init__(self, is_admin: bool | None = None) -> None:
+        """
+        Initializes the filter.
+
+        :param is_admin: True if the user is an administrator, False otherwise.
+        """
         self.is_admin = is_admin
 
     async def check(self, obj: Message | CallbackQuery) -> bool:
-        """Checks if the user is an administrator"""
+        """
+        Checks if the user is an administrator
+
+        :param obj: Message or CallbackQuery object from bot user.
+        :return: True if the user is an administrator, False otherwise.
+        """
         if self.is_admin is None:
             return False
         config: Config = obj.bot.get("config")
